a61570e50b86f22c0f5347886dc6c18eee8694bc
Liouchengkai <harry890409@icloud.com>
2025-10-14 19:44:26 +0800

    Add UTF-8 codec data for Unicode version 14.0.0

--- data/seed_mariadb.sql
+++ data/seed_mariadb.sql
@@ -1,3 +1,7 @@
+-- data/seed_mariadb.sql
+CREATE DATABASE IF NOT EXISTS market;
+USE market;
+
 CREATE TABLE IF NOT EXISTS prices_daily (
   dt DATE NOT NULL,
   symbol VARCHAR(32) NOT NULL,
@@ -11,9 +15,17 @@ CREATE TABLE IF NOT EXISTS prices_daily (
   PRIMARY KEY (dt, symbol)
 ) ENGINE=InnoDB;
 
+-- make the mart a passthrough of base table for now
+CREATE OR REPLACE VIEW prices_daily_mart AS
+SELECT dt, symbol, open, high, low, close, volume, vwap, is_trading_day
+FROM prices_daily;
+
+-- idempotent seed
 INSERT INTO prices_daily
-(dt, symbol, open, high, low, close, volume, vwap, is_trading_day)
+  (dt, symbol, open, high, low, close, volume, vwap, is_trading_day)
 VALUES
-('2024-01-02','2330.TW',1,1,1,1,10,1,1),
-('2024-01-03','2330.TW',1,1,1,1,11,1,1),
-('2024-01-04','2330.TW',1,1,1,1,12,1,1);
+  ('2024-01-02','2330.TW',1,1,1,1,10,1,1),
+  ('2024-01-03','2330.TW',1,1,1,1,11,1,1),
+  ('2024-01-04','2330.TW',1,1,1,1,12,1,1)
+ON DUPLICATE KEY UPDATE
+  close=VALUES(close), volume=VALUES(volume), vwap=VALUES(vwap);
